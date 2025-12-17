7272f342cc51e4de99e45fc676c2faa8f8f7f164
Nate Daines <nathaniel.daines@franklin.ai>
2025-12-17 17:05:56 +1100

    use neon http driver

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,6 +1,10 @@
+import 'dotenv/config';
 import { defineConfig } from "drizzle-kit";
 
 export default defineConfig({
   dialect: 'postgresql',
-  schema: './src/db/schema.ts'
+  schema: './src/db/schema.ts',
+  dbCredentials: {
+    url: process.env.DATABASE_URL!,
+  }
 })
